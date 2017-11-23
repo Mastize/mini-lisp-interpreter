7fc71e39573a0e63526dd7ce4c2ae84bccc21fc0
mastize <mastizenone@gmail.com>
2017-11-22 21:50:14 -0800

    evaluation done

--- lispy.c
+++ lispy.c
@@ -27,6 +27,32 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) return x + y;
+    if (strcmp(op, "-") == 0) return x - y;
+    if (strcmp(op, "*") == 0) return x * y;
+    if (strcmp(op, "/") == 0) return x / y;
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+    if (strstr(t->tag, "number")) {
+        return strtol(t->contents, NULL, 10);
+    }
+
+    char* op = t->children[1]->contents;
+    long x = eval(t->children[2]);
+
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char **argv) {
 
     // create some parsers
@@ -43,7 +69,7 @@ int main(int argc, char **argv) {
               "lispy    : /^/ <operator> <expr>+ /$/ ;",
               Number, Operator, Expr, Lispy);
 
-    puts("Lispy Version 0.0.0.0.1");
+    puts("Lispy Version 0.0.0.0.3");
     puts("Press Ctrl+c to Exit\n");
 
     while (1) {
@@ -54,7 +80,8 @@ int main(int argc, char **argv) {
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
