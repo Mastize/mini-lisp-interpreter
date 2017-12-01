7083c25ef428f41fd0891ff5a11af0f1187a2399
mastize <mastizenone@gmail.com>
2017-12-01 10:12:13 -0800

    read and print q-expr

--- lispy.c
+++ lispy.c
@@ -89,20 +89,25 @@ lval* lval_sexpr(void) {
     return v;
 }
 
+// create an q-expression type lval
+lval* lval_qexpr(void) {
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_QEXPR;
+    v->count = 0;
+    v->cell = NULL;
+    return v;
+}
+
 // free lval
 void lval_del(lval* v) {
 
     switch(v->type) {
-    case LVAL_NUM:
-        break;
+    case LVAL_NUM: break;
+    case LVAL_ERR: free(v->err); break;
+    case LVAL_SYM: free(v->sym); break;
 
-    case LVAL_ERR:
-        free(v->err);
-        break;
-    case LVAL_SYM:
-        free(v->sym);
-        break;
-
+        // If Qexpr or Sexpr then delete all elements inside
+    case LVAL_QEXPR:
     case LVAL_SEXPR:
         for (int i = 0; i < v->count; i++) {
             lval_del(v->cell[i]);
@@ -137,11 +142,14 @@ lval* lval_read(mpc_ast_t* t) {
     lval* x = NULL;
     if (strcmp(t->tag, ">") == 0) x = lval_sexpr();
     if (strstr(t->tag, "sexpr")) x = lval_sexpr();
+    if (strstr(t->tag, "qexpr")) x = lval_qexpr();
 
     // fill this list with any valid expression contained within
     for (int i = 0; i < t->children_num; i++) {
         if (strcmp(t->children[i]->contents, "(") == 0) continue;
         if (strcmp(t->children[i]->contents, ")") == 0) continue;
+        if (strcmp(t->children[i]->contents, "{") == 0) continue;
+        if (strcmp(t->children[i]->contents, "}") == 0) continue;
         if (strcmp(t->children[i]->tag, "regex") == 0) continue;
         x = lval_add(x, lval_read(t->children[i]));
     }
@@ -254,6 +262,9 @@ void lval_print(lval* v) {
         break;
     case LVAL_SEXPR:
         lval_expr_print(v, '(', ')');
+        break;
+    case LVAL_QEXPR:
+        lval_expr_print(v, '{', '}');
         break;
     }
 }
